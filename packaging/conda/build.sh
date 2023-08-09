bdb68b57ddc419ed7db858dd216c9a39e7d18b50
Popescu V <136721202+popescu-v@users.noreply.github.com>
2023-08-09 18:56:09 +0200

    WIP: First attempt at building osx-arm64 Conda packages

--- packaging/conda/build.sh
+++ packaging/conda/build.sh
@@ -11,19 +11,32 @@ cp $CWD/khiops_bin/packaging/{install,packaging}.cmake ./packaging/
 # On macOS, we have to build with the compiler outside conda. With teh conda's clang the following error occurs:
 # ld: unsupported tapi file type '!tapi-tbd' in YAML file '/Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/libSystem.tbd' for architecture x86_64
 # The binary location depends on the preset name used at the configure step (Cf. build.sh)
+#
+
+function copy_modl_binaries_to_miniconda_prefix_path () {
+    # Copy the MODL binaries to the Miniconda PREFIX path
+    local build_dir="$1"
+    local bin_dir="$2"
+    mkdir -p $PREFIX/$bin_dir
+    cp build/$build_dir/bin/MODL $PREFIX/$bin_dir
+    cp build/$build_dir/bin/MODL_Coclustering $PREFIX/$bin_dir
+}
+
 if [ "$(uname)" == "Darwin" ]
 then
     cmake --preset macos-clang-release -DBUILD_JARS=OFF -DTESTING=OFF -DCMAKE_CXX_COMPILER=/usr/bin/clang++
     cmake --build --preset macos-clang-release --parallel --target MODL MODL_Coclustering
-    BUILD_DIR="macos-clang-release"
+    copy_modl_binaries_to_miniconda_prefix_path "macos-clang-release" "bin"
+
+    # Cross-compile to ARM64
+    cmake --preset macos-clang-release -DCMAKE_OSX_ARCHITECTURES=arm64 -DBUILD_JARS=OFF -DTESTING=OFF -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+    cmake --build --preset macos-clang-release -DCMAKE_OSX_ARCHITECTURES=arm64 --parallel --target MODL MODL_Coclustering
+    # Also copy the ARM64 MODL binaries, which *override* the x86 binaries
+    copy_modl_binaries_to_miniconda_prefix_path "macos-clang-release" "khiops_arm64"
 else
     cmake --preset linux-gcc-release -DBUILD_JARS=OFF -DTESTING=OFF
     cmake --build --preset linux-gcc-release --parallel --target MODL MODL_Coclustering
-    BUILD_DIR="linux-gcc-release"
+    copy_modl_binaries_to_miniconda_prefix_path "linux-gcc-release" "bin"
 fi
-# Copy the MODL binaries to the anaconda PREFIX path
-mkdir -p $PREFIX/bin
-cp build/$BUILD_DIR/bin/MODL $PREFIX/bin
-cp build/$BUILD_DIR/bin/MODL_Coclustering $PREFIX/bin
 
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir --no-build-isolation -vvv
---
+++ packaging/conda/post-link.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+# On MAC OS, if on ARM, move relevant binaries to bin (overwrite X86 binaries)
+KHIOPS_ARM64_DIR="khiops_arm64"
+if [[ "$(uname)" == "Darwin" && "$(uname -p)" == "arm" ]]
+then
+    mv $PREFIX/$KHIOPS_ARM64_DIR/MODL $PREFIX/bin/
+    mv $PREFIX/$KHIOPS_ARM64_DIR/MODL_Coclustering $PREFIX/bin/
+    rm -fr $PREFIX/$KHIOPS_ARM64_DIR
+else
+    # Remove whole directory containing ARM-specific builds, if it exists
+    rm -fr $PREFIX/$KHIOPS_ARM64_DIR
+fi
+
