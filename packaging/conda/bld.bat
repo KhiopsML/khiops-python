b005dbba913dc1b18d564125d2a4a6abd2469910
Popescu V <136721202+popescu-v@users.noreply.github.com>
2023-08-11 17:54:52 +0200

    Depend on Khiops revision for building the Conda package
    related_to #70

--- packaging/conda/bld.bat
+++ packaging/conda/bld.bat
@@ -1,25 +1,34 @@
 REM Echo all output
 @echo on
-robocopy "%CWD%"\khiops_bin\src .\src /e
-robocopy "%CWD%"\khiops_bin\test .\test /e
-mkdir .\packaging
-robocopy "%CWD%"\khiops_bin\packaging\common .\packaging\common /e
-if errorlevel 8 exit 1
-copy /y "%CWD%"\khiops_bin\CMakeLists.txt .
-copy /y "%CWD%"\khiops_bin\CMakePresets.json .
-copy /y "%CWD%"\khiops_bin\LICENSE .
-copy /y "%CWD%"\khiops_bin\packaging\install.cmake .\packaging\
-copy /y "%CWD%"\khiops_bin\packaging\packaging.cmake .\packaging\
 
-% REM We use ninja for generator because VS does not work with conda build
-cmake --preset windows-msvc-release -G "Ninja" -DBUILD_JARS=OFF -DTESTING=OFF
+REM Clone Khiops sources
+git clone https://github.com/khiopsml/khiops.git khiops_bin
+cd .\khiops_bin\
+git checkout "%KHIOPS_REVISION%"
+cd ..
+
+REM Copy relevant Khiops files to current directory
+robocopy .\khiops_bin\src .\src /e
+robocopy .\khiops_bin\test .\test /e
+mkdir .\packaging
+robocopy .\khiops_bin\packaging\common .\packaging\common /e
+if errorlevel 8 exit 1
+copy /y .\khiops_bin\CMakeLists.txt .
+copy /y .\khiops_bin\CMakePresets.json .
+copy /y .\khiops_bin\LICENSE .
+copy /y .\khiops_bin\packaging\install.cmake .\packaging\
+copy /y .\khiops_bin\packaging\packaging.cmake .\packaging\
+
+REM Build the Khiops binaries
+cmake --preset windows-msvc-release -DBUILD_JARS=OFF -DTESTING=OFF
 cmake --build --preset windows-msvc-release --parallel --target MODL --target MODL_Coclustering
 
-REM Copy the MODL binaries to the anaconda PREFIX path
+REM Copy the MODL binaries to the Conda PREFIX path
 mkdir %PREFIX%\bin
 copy build\windows-msvc-release\bin\MODL.exe %PREFIX%\bin
 copy build\windows-msvc-release\bin\MODL_Coclustering.exe %PREFIX%\bin
 
+REM Build the Khiops Python package
 "%PYTHON%" -m pip install . --no-deps --ignore-installed --no-cache-dir --no-build-isolation -vvv
 
 if errorlevel 1 exit 1
--- packaging/conda/build.sh
+++ packaging/conda/build.sh
@@ -1,17 +1,24 @@
 # !/bin/bash
 
-# Echo all output
-set -x
+# Set-up the shell to behave more like a general-purpose programming language
+set -euo pipefail
 
+# Clone Khiops sources
+git clone --depth 1 https://github.com/khiopsml/khiops.git khiops_bin
+cd khiops_bin/
+git checkout ${KHIOPS_REVISION}
+cd ..
 
-cp -R $(pwd)/khiops_bin/{src,test} .
-cp -R $(pwd)/khiops_bin/packaging/common ./packaging/
-cp $(pwd)/khiops_bin/{CMakeLists.txt,CMakePresets.json,LICENSE} .
-cp $(pwd)/khiops_bin/packaging/{install,packaging}.cmake ./packaging/
+# Copy relevant Khiops files to current directory
+cp -R khiops_bin/{src,test} .
+cp -R khiops_bin/packaging/common ./packaging/
+cp khiops_bin/{CMakeLists.txt,CMakePresets.json,LICENSE} .
+cp khiops_bin/packaging/{install,packaging}.cmake ./packaging/
 
-# On macOS, we have to build with the compiler outside conda. With teh conda's clang the following error occurs:
+# Build the Khiops binaries
+# On macOS, we have to build with the compiler outside conda. With Conda's clang the following error occurs:
 # ld: unsupported tapi file type '!tapi-tbd' in YAML file '/Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/libSystem.tbd' for architecture x86_64
-# The binary location depends on the preset name used at the configure step (Cf. build.sh)
+# The binary location depends on the preset name used at the configure step
 if [ "$(uname)" == "Darwin" ]
 then
     cmake --preset macos-clang-release -DBUILD_JARS=OFF -DTESTING=OFF -DCMAKE_CXX_COMPILER=/usr/bin/clang++
@@ -22,9 +29,11 @@ else
     cmake --build --preset linux-gcc-release --parallel --target MODL MODL_Coclustering
     BUILD_DIR="linux-gcc-release"
 fi
-# Copy the MODL binaries to the anaconda PREFIX path
+
+# Copy the MODL binaries to the Conda PREFIX path
 mkdir -p $PREFIX/bin
 cp build/$BUILD_DIR/bin/MODL $PREFIX/bin
 cp build/$BUILD_DIR/bin/MODL_Coclustering $PREFIX/bin
 
+# Build the Khiops Python package
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir --no-build-isolation -vvv
