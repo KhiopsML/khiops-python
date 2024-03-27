6e0de658a66672ecfaa7adb050731cbaf3407704
Felipe Olmos <92923444+folmos-at-orange@users.noreply.github.com>
2024-03-27 10:03:58 +0100

    WIP host

--- packaging/conda/khiops/bld.bat
+++
@@ -1,7 +0,0 @@
-REM Echo all output
-@echo on
-
-REM Build the Khiops Python package
-"%PYTHON%" -m pip install . --no-deps --ignore-installed --no-cache-dir --no-build-isolation -vvv
-
-if errorlevel 1 exit 1
--- packaging/conda/khiops/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set-up the shell to behave more like a general-purpose programming language
-set -euo pipefail
-
-# Build the Khiops Python package in the base directory
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir --no-build-isolation -vvv
