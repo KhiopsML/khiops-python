6a87eacaebdaf09eeb20030ad0390af8db4ed0c4
Felipe Olmos <92923444+folmos-at-orange@users.noreply.github.com>
2025-01-13 16:16:13 +0100

    Make update_copyright ignore shebangs

--- scripts/update_copyright.py
+++ scripts/update_copyright.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # pylint: disable=line-too-long
 copyright_blob = f"""######################################################################################
-# Copyright (c) {datetime.today().year} Orange. All rights reserved.                                    #
+# Copyright (c) 2023-{datetime.today().year} Orange. All rights reserved.                               #
 # This software is distributed under the BSD 3-Clause-clear License, the text of     #
 # which is available at https://spdx.org/licenses/BSD-3-Clause-Clear.html or         #
 # see the "LICENSE.md" file for more details.                                        #
@@ -27,15 +27,25 @@ def update_copyright(file_path):
         lines = file.readlines()
     skipped_copyright = False
     with open(file_path, "w", encoding="utf8") as file:
-        file.write(copyright_blob)
-        for line in lines:
-            if line.startswith("#") and not skipped_copyright:
+        for line_number, line in enumerate(lines, start=1):
+            # Write any shebang
+            if line.startswith("#!") and line_number == 1:
+                file.write(line)
+            # Ignore a previous copyright banner
+            elif line.startswith("#") and not skipped_copyright:
                 continue
+            # After reading the old banner write the new and any line after
             elif not line.startswith("#") and not skipped_copyright:
                 skipped_copyright = True
+                file.write(copyright_blob)
                 file.write(line)
             elif skipped_copyright:
                 file.write(line)
+
+        # Write copyright if in not in skipped_copyright state at the end of the file
+        # This case is for files with only the banner (ex. __init__.py)
+        if not skipped_copyright:
+            file.write(copyright_blob)
 
 
 if __name__ == "__main__":
