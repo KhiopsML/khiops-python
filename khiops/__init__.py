5df6cdcee1c2e664b79b51085ab477e3b92ba749
Thierry RAMORASOAVINA <thierry.ramorasoavina@orange.com>
2026-03-05 12:46:25 +0100

    Update Pip and dev Conda package version to 11.0.0.3

--- khiops/__init__.py
+++ khiops/__init__.py
@@ -24,7 +24,7 @@ The available sub-modules inside the package are:
 """
 from khiops.core.internals.version import KhiopsVersion
 
-__version__ = "11.0.0.2"
+__version__ = "11.0.0.3"
 
 
 def get_compatible_khiops_version():
